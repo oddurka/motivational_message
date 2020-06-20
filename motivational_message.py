1c2ccc108a3ae4bdf7463e1c757b789f76fc4059
oddurka <oddurka@gmail.com>
2020-06-20 01:14:26 +0000

    adds the windows CMD specific display function

--- motivational_message.py
+++ motivational_message.py
@@ -12,21 +12,43 @@ def check_os():
         # Windows...
         return "Windows"
 
-# Gets the quote and author of the day from www.brainyquote.com, and returns them as a tuple
+# Gets the quote and author of the day from www.brainyquote.com, and returns them as a dict
 def get_quote():
     import requests
     from bs4 import BeautifulSoup
 
     URL = 'https://www.brainyquote.com/quote_of_the_day'
+    # Gets the html code for the given url
     page = requests.get(URL)
 
+    # Makes the data easier to interact with
     soup = BeautifulSoup(page.content, 'html.parser')
 
     results = soup.find("div", {"class": 'qll-bg'}).findAll("a")
-    quote = results[0].get_text()
-    author = results[1].get_text()
 
-    return (quote, author)
+    res = {
+        "quote": results[0].get_text(),
+        "author": results[1].get_text()
+    }
 
-print(get_quote())
+    return res
+
+#print(get_quote())
+
+
+# start a terminal window and displays the quote of the day
+def start_terminal_windows():
+    import os
+    quote = get_quote()
+
+    # Saves the quote to a txt file for easier display options
+    quoteFile = open('quoteFile.txt', 'w+')
+    quoteFile.write("### QUOTE OF THE DAY ###\n" + quote.get("quote") + '\n  -' + quote.get("author") + "\n #######################")
+    quoteFile.close()
+
+    # The CMD command
+    os.system("start type quoteFile.txt")
+
+start_terminal_windows()
+
 
